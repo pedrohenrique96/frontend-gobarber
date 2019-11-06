0ccdc670e948f7c40a03ef9d6eb6b9325b2bb184
Pedro Henrique <pedrohenrique@Pedros-iMac.local>
2019-11-05 22:36:59 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Pedro
+      </header>
+    </div>
+  );
+}
+
+export default App;
